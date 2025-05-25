7d2d2b7f052b8235e52567d6d7564f7d1e8b769c
Jim Pivarski <jpivarski@gmail.com>
2025-05-25 11:45:16 -0500

    JupyterLite's handling of imports is strange

--- site/files/data.py
+++ site/files/data.py
@@ -1,21 +1,8 @@
-from collections import defaultdict
-import json
-import pyodide.http
-import zipfile
-
-from tqdm import tqdm
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
-import shapely.geometry
-import shapely.ops
-
 URL_BASE = "https://pivarski-princeton.s3.us-east-1.amazonaws.com/us-election-analysis/"
 
 async def download(filename, base=URL_BASE):
+    import pyodide.http
+
     response = await pyodide.http.pyfetch(
         f"{base}{filename}", method="GET", cache="no-store", priority="high"
     )
